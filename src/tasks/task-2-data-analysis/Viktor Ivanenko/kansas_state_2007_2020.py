f6e9a9ce981c3f65bb72497bce944681e92d2fca
xxxVIKTORxxx <109345462+xxxVIKTORxxx@users.noreply.github.com>
2023-07-16 16:23:52 +0300

    Add files via upload

--- src/tasks/task-2-data-analysis/Viktor Ivanenko/kansas_state_2007_2020.py
+++ src/tasks/task-2-data-analysis/Viktor Ivanenko/kansas_state_2007_2020.py
@@ -4,18 +4,19 @@ import re
 
 path = 'src\data\cities crime data'
 files_list = os.listdir(path)
+files_list.remove('merged_kansas_state_2007_2020.csv')
 
 
 merged_kansas_state_2007_2020 = pd.DataFrame()
 
+
 for file in files_list:
     if '.csv' in str(file):
-
         city_file = pd.read_csv(path + '\\' + file)
         city_file['City name'] = file[:-4]
         merged_kansas_state_2007_2020 = pd.concat([merged_kansas_state_2007_2020, city_file])
 
-merged_kansas_state_2007_2020 = merged_kansas_state_2007_2020.iloc[:,:-1].fillna(0)
+merged_kansas_state_2007_2020 = merged_kansas_state_2007_2020.fillna(0)
 print("NaN columns: ", merged_kansas_state_2007_2020.isna().sum())
 print(files_list)
 print(merged_kansas_state_2007_2020)
@@ -24,12 +25,10 @@ for i in range(len(merged_kansas_state_2007_2020.columns)):
     column = i+1
     if column < len(merged_kansas_state_2007_2020.columns)-1:
         for row in range(len(merged_kansas_state_2007_2020.iloc[:, column])):
-            if row < len(merged_kansas_state_2007_2020.iloc[:, column])-1:
-                #print('column number is: ', column)
-                #print('number of row: ', row)
-                merged_kansas_state_2007_2020.iloc[row, column] = int(float(re.findall("\w*[\s\w.\s]*", str(merged_kansas_state_2007_2020.iloc[row, column]))[0]))
-            elif row == len(merged_kansas_state_2007_2020.iloc[:, column])-1:
-                merged_kansas_state_2007_2020.iloc[-1, column] = float(re.findall("\w*[\s\w.\s]*", str(merged_kansas_state_2007_2020.iloc[-1, column]))[0])
+            #print('column number is: ', column)
+            #print('number of row: ', row)
+            merged_kansas_state_2007_2020.iloc[row, column] = int(float(re.findall("\w*[\s\w.\s]*", str(merged_kansas_state_2007_2020.iloc[row, column]))[0]))
+
     else:
         break
 
@@ -39,6 +38,8 @@ for row in range(len(merged_kansas_state_2007_2020.iloc[:,0])):
         merged_kansas_state_2007_2020.iloc[row, 0] = str(merged_kansas_state_2007_2020.iloc[row, 0])[0:-13]
     else:
         break
+
+merged_kansas_state_2007_2020[merged_kansas_state_2007_2020['Type'] == 'City-Data.co'] = merged_kansas_state_2007_2020[merged_kansas_state_2007_2020['Type'] == 'City-Data.co'].replace('City-Data.co', 'Total')
 
 print(merged_kansas_state_2007_2020)
 
